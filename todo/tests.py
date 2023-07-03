93d11e66958d53c8fecf5f4d048cef38b9346548
1f10220210 <s1f102202101@iniad.org>
2023-07-04 03:04:28 +0900

    五回目の修正

--- todo/tests.py
+++ todo/tests.py
@@ -81,4 +81,4 @@ class TodoViewTestCase(TestCase):
             self.assertEqual(response.templates[0].name, 'todo/index.html')
             self.assertEqual(response.context['tasks'][0], task2)
             self.assertEqual(response.context['tasks'][1], task1)
-            
+  
