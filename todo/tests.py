d8b97cef04073896be67e3d8559c28501edc7351
1f10220210 <s1f102202101@iniad.org>
2023-07-04 03:02:02 +0900

    四回目の修正

--- todo/tests.py
+++ todo/tests.py
@@ -77,7 +77,8 @@ class TodoViewTestCase(TestCase):
             client = Client()
             response = client.get('/?order=post')
 
-            self.assertEqual(response.status_code,200)
+            self.assertEqual(response.status_code, 200)
             self.assertEqual(response.templates[0].name, 'todo/index.html')
             self.assertEqual(response.context['tasks'][0], task2)
             self.assertEqual(response.context['tasks'][1], task1)
+            
